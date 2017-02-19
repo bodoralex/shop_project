a801671336ce0435809c90c7622385fabc541f98
bodoralex <bodaleaa@gmail.com>
2017-02-19 13:30:23 +0100

    jetpult vectorized
    part II task 2.2

--- bolt/Bolt.java
+++ bolt/Bolt.java
@@ -1,48 +1,58 @@
 package bolt;
 
-import java.util.ArrayList;
+import java.util.Vector;
 
 public class Bolt {
 
 	protected String nev;
 	protected String cim;
 	protected String tulajdonos;
-	protected ArrayList<Tej> tejpult;
+	protected Vector<Tej> tejpult;
 	protected int flag;
-	
-	public Bolt(String nev, String cim, String tulajdonos, ArrayList<Tej> tejpult) {
+
+	public Bolt(String nev, String cim, String tulajdonos, Vector<Tej> tejpult) {
 		super();
 		this.nev = nev;
 		this.cim = cim;
 		this.tulajdonos = tulajdonos;
 		this.tejpult = tejpult;
-		
 	}
-	public void feltoltTej(Tej m){
+
+	public Bolt(String nev, String cim, String tulajdonos) {
+		super();
+		this.nev = nev;
+		this.cim = cim;
+		this.tulajdonos = tulajdonos;
+	}
+	
+	public void feltoltTej(Tej m) {
+		if(tejpult == null) tejpult = new Vector<Tej>();
 		tejpult.add(m);
 	}
-		
-	public boolean vanMegTej(){
-		if(tejpult == null || tejpult.size() == 0) return false;
+
+	public boolean vanMegTej() {
+		if (tejpult == null || tejpult.size() == 0)
+			return false;
 		return true;
 	}
-	public Tej vasarolTej(Tej m){
-		if(tejpult.contains(m)) {
+
+	public Tej vasarolTej(Tej m) {
+		if (tejpult.contains(m)) {
 			tejpult.remove(m);
 			return m;
 		}
 		System.out.println("A kért tej nem létezik.");
 		return null;
 	}
-	
+
 	public String getNev() {
 		return nev;
 	}
-	
+
 	public String getCim() {
 		return cim;
 	}
-	
+
 	public String getTulajdonos() {
 		return tulajdonos;
 	}
